d10fef22cd30b11a246547c8fe9e01a2e2ab394f
elango-meverse <elango29694@gmail.com>
2026-06-10 16:06:30 +0530

    Assignment_Week1_Day02

---
+++ varDecPrimDatatype.js
@@ -0,0 +1,29 @@
+//Variable
+// Declaration
+
+let firstName = "Elango"
+let mobileNumber = 9025155424
+let companyName = "Transunion"
+let isAutomation = true
+let hasPlaywright
+
+console.log("FirstName: " + firstName, "| Datatype: "  + typeof firstName)
+console.log("MobileNumber: " + mobileNumber, "| Datatype: " + typeof mobileNumber)
+console.log("companyName: " + companyName, "| Datatype: "  + typeof companyName)
+console.log("Automation knowleadge: " + isAutomation, "| Datatype: " + typeof isAutomation )
+console.log("Knows Playwright? " + hasPlaywright, "| Datatype: " + typeof hasPlaywright)
+
+console.log(firstName);
+console.log(typeof firstName);
+
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+console.log(companyName);
+console.log(typeof companyName);
+
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
